e1de16b7eb719ee4a3c8bd520203e2134c0c5e0e
fatherofsevenchildren <kudienko030303@mail.ru>
2024-10-05 22:49:31 +0300

    add Spring context and context.xml

--- ex00/SpringContext/src/main/java/edu/school21/application/Application.java
+++ ex00/SpringContext/src/main/java/edu/school21/application/Application.java
@@ -1,11 +1,7 @@
 package edu.school21.application;
 
-import edu.school21.preprocessor.PreProcessor;
-import edu.school21.preprocessor.PreProcessorToLowerImpl;
 import edu.school21.printer.Printer;
-import edu.school21.printer.PrinterWithDateTimeImpl;
 import edu.school21.printer.PrinterWithPrefixImpl;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
